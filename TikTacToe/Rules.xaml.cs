836967f75382aea7e5f1e9ea5be857aa66cc3b95
Marisa Wadsworth <10002444@student.toiohomai.ac.nz>
2020-03-06 13:20:55 +1300

    Initial Commit - Added Menu

---
+++ TikTacToe/Rules.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Media.SpeechSynthesis;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace TikTacToe
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Rules : Page
+    {
+        // Provides access to the speech synthesis engine(voice) for Text-to-speech
+        SpeechSynthesizer reader;
+        public Rules()
+        {
+            this.InitializeComponent();
+            // Configure the audio output.
+            reader = new SpeechSynthesizer();
+        }
+
+        private void Button_Click_Speak(object sender, RoutedEventArgs e)
+        {
+            // Talk the text
+            Talk(Speak_Block.Text);
+        }
+
+        /// <summary>
+        /// Plays a text to talk
+        /// </summary>
+        /// <param name="message">Our message to talk</param>
+        private async void Talk(string message)
+        {
+            // Get the text
+            var stream = await reader.SynthesizeTextToStreamAsync(message);
+            // Setup the stream for the player
+            media.SetSource(stream, stream.ContentType);
+            // Play the message
+            media.Play();
+        }
+
+        private void Button_Click_Back(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(StartPage));
+        }
+
+    }
+}
---
+++ TikTacToe/StartPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace TikTacToe
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class StartPage : Page
+    {
+        public StartPage()
+        {
+            this.InitializeComponent();
+        }
+
+        // Takes you to the TicTacToe Game
+        private void Button_Click_Play(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
+        }
+
+        // Takes you to the Rules page
+        private void Button_Click_Rules(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Rules));
+        }
+
+        // Takes you to the Classes page
+        private void Button_Click_Classes(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Class));
+        }
+    }
+}
